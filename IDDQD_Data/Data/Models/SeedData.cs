c14aea39c43038634474baba36d475606e5059e3
MJW3BB <mjwebb2@buffs.wtamu.edu>
2020-04-17 22:36:18 -0500

    merged technical with dev

--- IDDQD_Data/Data/Models/SeedData.cs
+++ IDDQD_Data/Data/Models/SeedData.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace IDDQD.Data
+namespace IDDQD_Data.Data.Models
 {
     public static class SeedData
     {
