53d88656c5c1661fb06f0af62f127b6b2cfd0767
Haley <hksmith2@buffs.wtamu.edu>
2020-04-20 22:21:50 -0500

    Added migrations and identity db

--- IDDQD_UI/Startup.cs
+++ IDDQD_UI/Startup.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Net; // Droplet Use
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.HttpOverrides; // Droplet Use
@@ -18,6 +18,8 @@ using Pomelo.EntityFrameworkCore.MySql;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using IDDQD.Middleware;
 using IDDQD_Repo.DependencyInjection;
+using IDDQD_Data.Data.Models; 
+using IDDQD.Areas.Identity.Data; 
 
 namespace IDDQD
 {
@@ -33,30 +35,96 @@ namespace IDDQD
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            /*
-        services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlite(
+            services.AddDbContext<IDDQDIdentityDbContext>(options =>
+            options.UseSqlServer(
                 Configuration.GetConnectionString("DefaultConnection")));
-        services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            .AddEntityFrameworkStores<ApplicationDbContext>();
-        services.AddDbContext<SKLContext>(options =>
-        options.UseSqlite(Configuration.GetConnectionString("SkillContext")));
-        
-        services.AddDbContext<DISPContext>(options =>
-        options.UseSqlite(Configuration.GetConnectionString("DispositionContext")));
-        
-        services.AddDbContext<KNEContext>(options =>
-        options.UseSqlite(Configuration.GetConnectionString("KnowledgeContext")));
-          */
-
-        services.AddRazorPages();
-
-        //add database and add UnitOfWork using Wizard Context
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<IDDQDIdentityDbContext>();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+                //add database and add UnitOfWork using Wizard Context
             services.AddDbContext<CDKSTContext>(
                 options => options.UseMySql(Configuration.GetConnectionString("IDDQD_MYSQL_CONNECTION"),
                                             mySqlOptions => mySqlOptions.ServerVersion(new Version(5, 7, 29), ServerType.MySql)
-                )).AddUnitOfWork<CDKSTContext>();            
+                )).AddUnitOfWork<CDKSTContext>();  
+                    services.ConfigureApplicationCookie(options =>
+    {
+        // Cookie settings
+        options.Cookie.HttpOnly = true;
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+        options.LoginPath = "/Identity/Account/Login";
+        options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+        options.SlidingExpiration = true;
+    });
+        }
+
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseDatabaseErrorPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+                
+            endpoints.MapRazorPages();
+        });
+            // Droplet Use
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            // app.UseHttpsRedirection();
+            // app.UseStaticFiles();
+            // app.UseRouting();
+            // app.UseAuthentication();
+            // app.UseAuthorization();
+            // app.UseEndpoints(endpoints =>
+            // {
+            //     endpoints.MapRazorPages();
+            // });
+}
+      
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+
+
+      
+
+          
 
     //     services.Configure<IdentityOptions>(options =>
     //     {
@@ -78,46 +146,8 @@ namespace IDDQD
     //     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
     //     options.User.RequireUniqueEmail = false;
     // });
-    services.ConfigureApplicationCookie(options =>
-    {
-        // Cookie settings
-        options.Cookie.HttpOnly = true;
-        options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
 
-        options.LoginPath = "/Identity/Account/Login";
-        options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-        options.SlidingExpiration = true;
-    });
 }
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            // Droplet Use
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            });
+      
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
-        }
-    }
 }
