503b794dc54640d25d8df79cbb372b97fca09d2b
MJW3BB <mjwebb2@buffs.wtamu.edu>
2020-04-20 19:38:50 -0500

    merged scrum master with UI

--- IDDQD_UI/Startup.cs
+++ IDDQD_UI/Startup.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Net; // Droplet Use
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.HttpOverrides; // Droplet Use
@@ -32,22 +33,22 @@ namespace IDDQD
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-             services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(
-            Configuration.GetConnectionString("DefaultConnection")));
-    services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-        .AddEntityFrameworkStores<ApplicationDbContext>();
-    services.AddRazorPages();
 
-    services.Configure<IdentityOptions>(options =>
-    {
-        // Password settings.
-        options.Password.RequireDigit = true;
-        options.Password.RequireLowercase = true;
-        options.Password.RequireNonAlphanumeric = true;
-        options.Password.RequireUppercase = true;
-        options.Password.RequiredLength = 6;
-        options.Password.RequiredUniqueChars = 1;
+            /*
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(
+                Configuration.GetConnectionString("DefaultConnection")));
+        services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+        services.AddDbContext<SKLContext>(options =>
+        options.UseSqlite(Configuration.GetConnectionString("SkillContext")));
+        
+        services.AddDbContext<DISPContext>(options =>
+        options.UseSqlite(Configuration.GetConnectionString("DispositionContext")));
+        
+        services.AddDbContext<KNEContext>(options =>
+        options.UseSqlite(Configuration.GetConnectionString("KnowledgeContext")));
+          */
 
         services.AddRazorPages();
 
@@ -67,12 +68,16 @@ namespace IDDQD
     //     options.Password.RequiredLength = 6;
     //     options.Password.RequiredUniqueChars = 1;
 
-        // User settings.
-        options.User.AllowedUserNameCharacters =
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-        options.User.RequireUniqueEmail = false;
-    });
+    //     // Lockout settings.
+    //     options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    //     options.Lockout.MaxFailedAccessAttempts = 5;
+    //     options.Lockout.AllowedForNewUsers = true;
 
+    //     // User settings.
+    //     options.User.AllowedUserNameCharacters =
+    //     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    //     options.User.RequireUniqueEmail = false;
+    // });
     services.ConfigureApplicationCookie(options =>
     {
         // Cookie settings
@@ -82,10 +87,10 @@ namespace IDDQD
         options.LoginPath = "/Identity/Account/Login";
         options.AccessDeniedPath = "/Identity/Account/AccessDenied";
         options.SlidingExpiration = true;
-    });    
-        }
-                 // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-              public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    });
+}
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -112,10 +117,7 @@ namespace IDDQD
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
-        });
+            });
+        }
     }
-
-}
-   
-  
 }
