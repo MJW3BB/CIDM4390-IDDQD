59d0475e6f325f59dee5989791e56a755b3dcacc
Dubs Lasley <dubs6013@gmail.com>
2020-04-26 19:45:16 -0500

    Fixed some additional links to pages, added and removed some code

---
+++ EmailVer/EmailVer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using IDDQD.Data;
+using IDDQD.Services;
+using IDDQD.Areas.Identity;
+using IDDQD.Areas.Identity.Data;
+using IDDQD.Areas.Identity.Pages;
+
+namespace EmailVer
+{
+    public class EmailVer
+    {
+        private readonly EmailVer _emailver;
+
+        public EmailVerTest()
+        {
+            _emailver = new EmailVer();
+        }   
+        [Fact]
+        public void IsValid_ValidEmail_ReturnsTrue()
+        {
+            Regex regex = new Regex(@"^[\w0-9._%+-]+@[\w0-9.-]+\.[\w]{2,6}$");
+            return regex.IsMatch(emailAddress);
+        }
+    }
+}
